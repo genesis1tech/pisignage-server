672fdf763646a8ce26d7fea89a3097048d43f3f4
santhoshmaderi <santhosh@colloqi.com>
2025-12-09 13:12:05 +0530

    file-util.js
    Commented sanitization

--- app/others/file-util.js
+++ app/others/file-util.js
@@ -1,42 +1,51 @@
-var fs = require('fs'),
-    sanitizeHtml = require('sanitize-html'),
-    path = require('path'),
-    
-    library = '\n<script src="../piSignagePro/templates/screen.min.js"></script>\n';
+import fs from 'fs/promises';
+import sanitizeHtml from 'sanitize-html';
+import path from 'path';
 
-exports.modifyHTML = function(assetsDir,templateName){
-	var closingBodyIndex,
-        modifiedData,
-        sanitize,
-        templatePath;
+const library = '\n<script src="../piSignagePro/templates/screen.min.js"></script>\n';
 
-    if (!templateName)
+
+// In old code sanitized data was not written back to the file, hence adding only library to the file. Need to check 
+// In server2 no sanitization is done.
+
+export const modifyHTML = async (assetsDir, templateName) => {
+    if (!templateName) {
         return;
+    }
 
-    templatePath = path.join(assetsDir, templateName);
+    const templatePath = path.join(assetsDir, templateName);
 
-    fs.readFile(templatePath,'utf8',function(err,data){
-        if(err)
-           return console.log('error','custom_layout File Read Error',err)
-        //remove script tags
-        sanitize= sanitizeHtml(data,{
-                allowedTags: ['!DOCTYPE','html','head','meta','title','body','h1','h2','h3', 'h4', 'h5', 'h6', 
-                    'blockquote', 'p', 'a', 'ul', 'ol','nl', 'li', 'b', 'i', 'strong', 'em', 'strike', 'code', 
-                    'hr', 'br', 'div','table', 'thead', 'caption', 'tbody', 'tr', 'th', 'td', 'pre' , 
-                    'marquee', 'style', 'iframe','link','script','img'
-                ],
-                allowedAttributes: false
-        });
+    try {
+        // Read the HTML file
+        const data = await fs.readFile(templatePath, 'utf8');
 
-        // insert css and js files
-        //sanitize = '<!DOCTYPE html>'+sanitize;
-        sanitize = data;
-        closingBodyIndex = sanitize.lastIndexOf('</body>');
-        modifiedData = sanitize.slice(0,closingBodyIndex)+library+sanitize.slice(closingBodyIndex);
+        // Optional: Apply sanitization if needed
+        /* const sanitizedData = sanitizeHtml(data, {
+            allowedTags: [
+                '!DOCTYPE', 'html', 'head', 'meta', 'title', 'body', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
+                'blockquote', 'p', 'a', 'ul', 'ol', 'nl', 'li', 'b', 'i', 'strong', 'em', 'strike', 'code',
+                'hr', 'br', 'div', 'table', 'thead', 'caption', 'tbody', 'tr', 'th', 'td', 'pre',
+                'marquee', 'style', 'iframe', 'link', 'script', 'img'
+            ],
+            allowedAttributes: false
+        }); */
+
+        // Find </body> tag
+        const closingBodyIndex = data.lastIndexOf('</body>');
         
-        fs.writeFile(templatePath,modifiedData,function(err){
-            // template modification successful
-        })
-       
-    })
-}
+        if (closingBodyIndex === -1) {
+            console.warn(`No </body> tag found in ${templateName}, skipping modification`);
+            return;
+        }
+
+        // Inject library before </body>
+        const modifiedData = data.slice(0, closingBodyIndex) + library + data.slice(closingBodyIndex);
+
+        // Write back to file
+        await fs.writeFile(templatePath, modifiedData, 'utf8');
+        console.log(`Successfully modified ${templateName}`);
+    } catch (err) {
+        console.error(`Error modifying HTML template ${templateName}:`, err);
+        throw err;
+    }
+};
